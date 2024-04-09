5bb77b3ff94dd23187ef816fefc75cd13947ad42
Alexander Smirnov <aleksander50.500@gmail.com>
2024-04-09 23:20:24 +0300

    add 5lab

--- featured_post.php
+++ featured_post.php
@@ -1,16 +1,16 @@
 <a href="/post?id=<?= $post['id'] ?>">
     <div class="big-card">
-        <img src="<?= $post['back-img'] ?>" alt="<?= $post['title'] ?>">
+        <img src="<?= $post['image_url'] ?>" alt="<?= $post['title'] ?>">
         <div class="big-card_conteiner">
             <p class="big-card__title"><?= $post['title'] ?></p>
             <p class="big-card__title__under">
                 <?= $post['subtitle'] ?>
             </p>
             <div class="big-card__footer__conteiner">
-                <img class="big-card-img" src=<?= $post['img_modifier'] ?> alt="Men1">
+                <img class="big-card-img" src=<?= $post['author_url'] ?> alt="Men1">
                 <div class="big-card-footer_block">
                     <p class="big-card-name"><?= $post['author'] ?></p>
-                    <p class="big-card-date"><?= $post['date'] ?></p>
+                    <p class="big-card-date"><?= $post['publish_date'] ?></p>
                 </div>
             </div>
         </div>
--- home.php
+++ home.php
@@ -1,93 +1,30 @@
-<!-- 
-// $featured_post = [
-//     [
-//         'id' => 1,
-//         'title' => 'The Road Ahead',
-//         'subtitle' => 'The road ahead might be paved - it might not be.',
-//         'img_modifier' => 'http://localhost:8001/static/images/img-home/Cards/IMAGEMen1.png',
-//         'author' => 'Mat Vogels',
-//         'date' => date("F d, Y", 1443139200),//использовать time stamp
-//         'title-btn' => 'Photography',
-//         'class-color-btn' => 'photography',
-//         'back-img' => 'http://localhost:8001/static/images/img-home/TheRoadAhead.png',
-//     ],
-//     [
-//         'id' => 2,
-//         'title' => 'From Top Down',
-//         'subtitle' => 'Once a year, go someplace you’ve never been before.',
-//         'img_modifier' => 'http://localhost:8001/static/images/img-home/Cards/IMAGEMen2.png',
-//         'author' => 'William Wong',
-//         'date' => date("F d, Y", 1443139200),
-//         'title-btn' => 'Adventure',
-//         'class-color-btn' => 'adventure',
-//         'back-img' => 'http://localhost:8001/static/images/img-home/FromTopDown.png',
-//     ],
-// ];
-//
-
-//
-// $most_recent_post = [
-//     [
-//         'id' => 1,
-//         'title' => 'Still Standing Tall',
-//         'subtitle' => 'Life begins at the end of your comfort zone.',
-//         'img_modifier' => 'http://localhost:8001/static/images/img-home/Cards/IMAGEMen2.png',
-//         'author' => 'William Wong',
-//         'date' => date("m/d/Y", 1443139200),
-//         'back-img' => 'http://localhost:8001/static/images/img-home/Cards/IMAGE1.png',
-//     ],
-//     [
-//         'id' => 2,
-//         'title' => 'Sunny Side Up',
-//         'subtitle' => 'No place is ever as bad as they tell you it’s going to be.',
-//         'img_modifier' => 'http://localhost:8001/static/images/img-home/Cards/IMAGEMen1.png',
-//         'author' => 'Mat Vogels',
-//         'date' => date("m/d/Y", 1443139200),
-//         'back-img' => 'http://localhost:8001/static/images/img-home/Cards/IMAGE2.png',
-//     ],
-//     [
-//         'id' => 3,
-//         'title' => 'Water Falls',
-//         'subtitle' => 'We travel not to escape life, but for life not to escape us.',
-//         'img_modifier' => 'http://localhost:8001/static/images/img-home/Cards/IMAGEMen1.png',
-//         'author' => 'Mat Vogels',
-//         'date' => date("m/d/Y", 1443139200),
-//         'back-img' => 'http://localhost:8001/static/images/img-home/Cards/IMAGE3.png',
-//     ],
-//     [
-//         'id' => 4,
-//         'title' => 'Through the Mist',
-//         'subtitle' => 'Travel makes you see what a tiny place you occupy in the world.',
-//         'img_modifier' => 'http://localhost:8001/static/images/img-home/Cards/IMAGEMen2.png',
-//         'author' => 'William Wong',
-//         'date' => date("m/d/Y", 1443139200),
-//         'back-img' => 'http://localhost:8001/static/images/img-home/Cards/IMAGE4.png',
-//     ],
-//     [
-//         'id' => 5,
-//         'title' => 'Awaken Early',
-//         'subtitle' => 'Not all those who wander are lost.',
-//         'img_modifier' => 'http://localhost:8001/static/images/img-home/Cards/IMAGEMen1.png',
-//         'author' => 'Mat Vogels',
-//         'date' => date("m/d/Y", 1443139200),
-//         'back-img' => 'http://localhost:8001/static/images/img-home/Cards/IMAGE5.png',
-//     ],
-//     [
-//         'id' => 6,
-//         'title' => 'Try it Always',
-//         'subtitle' => 'The world is a book, and those who do not travel read only one page.',
-//         'img_modifier' => 'http://localhost:8001/static/images/img-home/Cards/IMAGEMen1.png',
-//         'author' => 'Mat Vogels',
-//         'date' => date("m/d/Y", 1443139200),
-//         'back-img' => 'http://localhost:8001/static/images/img-home/Cards/IMAGE6.png',
-//     ],
-// ]; -->
 <?php
-  require_once('./dbconnect.php');
-  $dbConnection = createDBConnection();
-  $featured_post = getPostsFromDB($dbConnection, 1);
-  $most_recent_post = getPostsFromDB($dbConnection, 0);
+  const HOST = 'localhost';
+  const USERNAME = 'root';
+  const PASSWORD = '';
+  const DATABASE = 'blog';
+  function createDBConnection(): mysqli {
+    $conn = new mysqli(HOST, USERNAME, PASSWORD, DATABASE);
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    return $conn;
+  }
+  
+  function closeDBConnection(mysqli $conn): void {
+    $conn->close();
+  }
+  
+  $conn = createDBConnection();
+    $sql = "SELECT * FROM post";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+      $posts = $result->fetch_all(MYSQLI_ASSOC);
+    }
+  closeDBConnection($conn)
 ?>
+
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -95,10 +32,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="http://localhost:8001/static/styles/home.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&family=Oxygen:wght@300;400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./styles/home.css">
+    <link rel="preconnect" href="http://fonts.googleapis.com">
+    <link rel="preconnect" href="http://fonts.gstatic.com" crossorigin>
+    <link href="http://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&family=Oxygen:wght@300;400;700&display=swap" rel="stylesheet">
     <title>Lab_4</title>
 </head>
 
@@ -166,10 +103,12 @@
                 <hr class="line">
             </div>
             <nav class="feature-posts-cards">
-                <?php
-                foreach ($featured_post as $post) {
-                    include 'featured_post.php';
-                }
+                <?php 
+                    foreach ($posts as $post) {
+                        if($post['featured'] == 1){
+                            include 'featured_post.php';
+                        }        
+                    }
                 ?>
 
             </nav>
@@ -181,9 +120,11 @@
             </div>
             <nav class="most-recent_cards">
                 <?php
-                foreach ($most_recent_post as $post) {
-                    include 'most_recent_post.php';
-                }
+                    foreach ($posts as $post) {
+                        if($post['featured'] != 1){
+                            include 'most_recent_post.php';
+                        }        
+                    }
                 ?>
             </nav>
         </div>
